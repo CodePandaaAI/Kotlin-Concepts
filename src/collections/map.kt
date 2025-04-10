f6cb5a98a67b204a10029fd46945616bddd8c365
CodePandaaAI <romitsharmakv@gmail.com>
2025-04-10 15:37:51 +0530

    Day 1 : Kotlin Concepts
    Practicing Map, Set

--- src/collections/map.kt
+++ src/collections/map.kt
@@ -1,7 +1,7 @@
 package collections
 
 fun main() {
-    val studentMarks = mutableMapOf("Romit" to 85, "Aman" to 90, "Priya" to 78)
+    val studentMarks = mutableMapOf("Romit" to 85, "Aman" to 90, "Priya" to 78) // Key-Value Pairs List
 
     println("Original Map:")
     println(studentMarks)
@@ -9,22 +9,29 @@ fun main() {
     val newStudent = "Ramesh"
     val newMarks = 66
 
+    // Checks if newStudent doesn't exist, adds a newStudent "Ramesh"
     if(!studentMarks.containsKey(newStudent)){
         studentMarks[newStudent] = newMarks
 
         println("After adding new student $newStudent")
-        println(studentMarks)
+        println(studentMarks) // list after updating list
     }
     val updateStudent = "Romit"
     val updatedMarks = 92
+
+    // updating existing student "Romit" Marks if the student exists
+
     if(studentMarks.containsKey(updateStudent)){
         studentMarks[updateStudent] = updatedMarks
     }
 
     val deleteStudent = "Priya"
+    // deleting a student from the map if it exists
+
     if(studentMarks.containsKey(deleteStudent)){
         studentMarks.remove(deleteStudent)
         println("After Deleting Student $deleteStudent")
+        // printed the list after deleting student
         println(studentMarks)
     }
 
--- src/collections/set.kt
+++ src/collections/set.kt
@@ -1,2 +1,19 @@
 package collections
 
+fun main() {
+    val fruits = mutableSetOf("Apple", "Banana", "Mango")
+
+    println("Original Set: $fruits")
+
+    // Trying to add duplicate
+    fruits.add("Banana")
+    println("After trying to add duplicate 'Banana': $fruits") // No change
+
+    // Adding new item
+    fruits.add("Orange")
+    println("After adding 'Orange': $fruits")
+
+    // Removing item
+    fruits.remove("Mango")
+    println("After removing 'Mango': $fruits")
+}
